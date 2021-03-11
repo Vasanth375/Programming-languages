cea2f58f28120ead02b2fc367e9805c0bd6beae0
Vasanth375 <77767837+Vasanth375@users.noreply.github.com>
2021-03-11 20:59:24 +0530

    Create Add.cs

---
+++ C#/Add.cs
@@ -0,0 +1,38 @@
+using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Text.RegularExpressions;
+
+ // Compiler version 4.0, .NET Framework 4.5
+
+ namespace Dcoder
+
+ {
+
+   public class Program
+
+   {
+
+     public static void Main(string[] args)
+
+     {
+
+       //declaring two variables
+
+       int num1=2,num2=4;
+
+       
+
+       //adding and printing
+
+       Console.WriteLine("result "+(num1+num2));
+
+     }
+
+   }
+
+ }
+
