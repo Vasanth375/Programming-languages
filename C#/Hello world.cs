f8fea27735ceb2217fd2f0557ad65c55c5f6bcb1
Vasanth375 <77767837+Vasanth375@users.noreply.github.com>
2021-03-11 12:08:53 +0530

    Create Hello world.cs

---
+++ C#/Hello world.cs
@@ -0,0 +1,30 @@
+using System;
+
+ // Compiler version 4.0, .NET Framework 4.5
+
+ namespace Dcoder
+
+ {
+
+   class Program
+
+   {
+
+     static void Main()
+
+     {
+
+       string name=Console.ReadLine();
+
+       //input to name
+
+       Console.WriteLine("Hello, Dcoder! "+name);
+
+       //output statement
+
+     }
+
+   }
+
+ }
+
