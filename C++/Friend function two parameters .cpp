21c6367847f7ae444a548c05cac1b949a4e1de5f
Vasanth <vasanthnimmala69@outlook.com>
2021-04-08 23:26:07 +0530

    Create Friend function two parameters .cpp

---
+++ C++/Friend function two parameters .cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+//forward declaration
+class classB;
+
+class classA
+{
+  //private data and functions
+  int a=4;
+  void display();
+  
+  //friend function of classA and classB as parameters
+  friend void add(classA,classB);
+
+};
+
+void classA::display()
+  {
+    cout<<"enter a ";//cin>>a;
+  }
+  
+class classB
+{
+  //private data and functions
+  int b=6;
+  void display()
+  {
+    cout<<"enter b ";//cin>>b;
+  }
+  
+  //friend function of classA and classB as parameters
+  friend void add(classA,classB);
+
+};
+
+//friend function definition
+void add(classA A,classB B)
+{
+  A.display();
+  B.display();
+  cout<<A.a+B.b;
+}
+
+int main()
+{
+  classA A;
+  classB B;
+  add(A,B);
+}
