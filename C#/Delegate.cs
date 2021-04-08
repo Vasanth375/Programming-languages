344eb444b6645429260475de9a737b2bea4cd376
Vasanth <vasanthnimmala69@outlook.com>
2021-04-08 10:19:03 +0530

    Create Delegate.cs

---
+++ C#/Delegate.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+ // Compiler version 4.0, .NET Framework 4.5
+
+
+ namespace Dcoder
+ {
+   //declaration of delegate
+   delegate void even();
+   
+   //class one
+   public class Program
+   {
+     
+     void fun()
+     {
+       Console.WriteLine("hello fun()");
+     }
+     
+    void display()
+    {
+       Console.WriteLine("hello display()");
+    }
+    
+     public static void Main(string[] args)
+     {
+       //creating object for class one
+       Program p = new Program();
+       
+       //creating delegate object added fun()
+       even e = new even(p.fun);
+       
+       //adding display() to delegate object
+       e+=p.display;
+       
+       //calling both methods at once
+       e();
+       
+       //creating object for class two
+       csn c=new csn();
+       
+       //calling public function of class two
+       c.function();
+     }
+     
+   }
+   
+   //class two
+   class csn
+   {
+     
+     void fun1()
+     {
+       Console.WriteLine("hello fun1()");
+     }
+     
+     public void function()
+     {
+       csn c=new csn();
+       even e = new even(c.fun1);
+       e();
+     }
+     
+   }
+ }
