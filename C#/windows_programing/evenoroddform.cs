1c87a0057b31887f0811e43338e9664d6c2a4e1f
Vasanth <vasanthnimmala69@outlook.com>
2021-06-16 18:46:26 +0530

    Create evenoroddform.cs

---
+++ C#/windows_programing/evenoroddform.cs
@@ -0,0 +1,45 @@
+/*Program to check even or odd number */
+//before excecuting create design time label , button and textbox  
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int num = Convert.ToInt32(textBox1.Text);
+            if (num % 2 == 0)
+            {
+                MessageBox.Show("EVEN NUMBER");
+            }
+            else
+            {
+                MessageBox.Show("ODD NUMBER");
+            }
+        }
+    }
+}
