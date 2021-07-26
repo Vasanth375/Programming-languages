4a96916e60a97eb059c677c883e652f12fc44921
Vasanth <vasanthnimmala69@outlook.com>
2021-07-26 20:25:42 +0530

    Create DivideByZeroException.cs
    C# performing DivideByZeroException

---
+++ C#/DivideByZeroException.cs
@@ -0,0 +1,14 @@
+using System;
+namespace ConsoleApplication7
+{
+    class main
+    {
+        static void Main()
+        {
+            int a = 12;
+            Console.WriteLine("Performing Divide by zero exception");
+            Console.WriteLine(a / 0);
+            Console.ReadKey();
+        }
+    }
+}
